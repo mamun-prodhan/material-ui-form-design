597198621d524effeb8214eb530176e69ff98568
mamun-prodhan <mamunpr9@gmail.com>
2023-02-03 09:03:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography gutterBottom variant='h3' align='center'>
+        <Box sx={{ fontWeight: "bold" }}>React App</Box>
+      </Typography>
+
+      <Card style={{ maxwidth: "750px", margin: "0 auto", padding: "20px 5px" }}>
+        <CardContent>
+          <form>
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={4} item>
+                <TextField
+                  placeholder='Enter First Name'
+                  label="First Name"
+                  variant='outlined'
+                  fullWidth
+                  required />
+              </Grid>
+              <Grid xs={12} sm={4} item>
+                <TextField label="Middle Name" placeholder='Enter Middle Name' variant='outlined' fullWidth required />
+              </Grid>
+              <Grid xs={12} sm={4} item>
+                <TextField label="Last Name" placeholder='Enter Last Name' variant='outlined' fullWidth required />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField type="email" label="Email" placeholder='Enter email address' variant='outlined' fullWidth required />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField type="number" label="Phone" placeholder='Enter phone number' variant='outlined' fullWidth required />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant='outlined' fullWidth required />
+              </Grid>
+              <Grid xs={12} item>
+                <Button type='submit' variant="contained" color="primary" fullWidth>Submit</Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
